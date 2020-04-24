05cd5c2096b59dddd69ba46432a91dc54eb43fbd
susumoa <tobiasbori@gmail.com>
2020-04-24 14:10:09 +0100

    feat create get and post route

--- README.md
+++ README.md
@@ -1,10 +1,20 @@
 # Weather-Journal App Project
 
 ## Overview
-This project requires you to create an asynchronous web app that uses Web API and user data to dynamically update the UI. 
+
+This project requires you to create an asynchronous web app that uses Web API and user data to dynamically update the UI.
+
+## To get started
+
+- clone project with `https://github.com/susumoa/mobile-flashcards.git`
+- install Node.js
+- install express, body-parser and cors with `npm install express body-parser cors`
+- start the server with `node server.js`
 
 ## Instructions
+
 This will require modifying the `server.js` file and the `website/app.js` file. You can see `index.html` for element references, and once you are finished with the project steps, you can use `style.css` to style your application to customized perfection.
 
 ## Extras
+
 If you are interested in testing your code as you go, you can use `tests.js` as a template for writing and running some basic tests for your code.
--- server.js
+++ server.js
@@ -1,19 +1,44 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+projectData = [];
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
+const bodyParser = require('body-parser');
 //Here we are configuring express to use body-parser as middle-ware.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
-
 // Setup Server
+const port = 8000;
+const server = app.listen(port, () => {
+  console.log(`Running on localhost: ${port}`);
+});
+
+// GET route
+app.get('/', (req, res) => {
+  res.send(projectData);
+});
+
+// POST route
+app.post('/', (req, res) => {
+  let data = req.body;
+  let newEntry = {
+    temperature: data.temperature,
+    data: data.date,
+    userResponse: data.userResponse,
+  };
+  console.log(newEntry);
+  projectData.push(newEntry);
+});
